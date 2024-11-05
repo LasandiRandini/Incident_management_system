ac149acd7d0d7f04ff45cdef72d42ce9b7e98d8c
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2024-11-05 20:33:14 +0530

    Refactor import statement for useState in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
-import { useState } from "react";
+import  useState  from "react";
 
 
 import Dashboardpage from "./pages/dashboard";
